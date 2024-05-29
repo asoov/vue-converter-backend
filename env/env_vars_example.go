1925f73c5c4edce85826e975f3bcc601c1666326
Adrian Soovary <adrian.soovary@gmail.com>
2024-05-29 13:13:13 +0200

    devex: give an example file for the env var usage

---
+++ env/env_vars_example.go
@@ -0,0 +1,12 @@
+package env
+
+import "os"
+
+func _InitializeEnvVars() {
+	// Define the environment variables
+	// This is a placeholder for the actual environment variables
+	// that you would define in your application
+	// For example, you might define a variable for the port
+	// that the server should listen on
+	os.Setenv("OAI_KEY", "set your personal OAI key here")
+}
